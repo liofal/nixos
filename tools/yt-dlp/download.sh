ad1470f962546fe3a7851995618625045ffa0a20
Liofal <lfa@gmx.com>
2026-01-23 08:49:33 +0100

    yt-dlp: use stable output template

--- tools/yt-dlp/download.sh
+++ tools/yt-dlp/download.sh
@@ -8,14 +8,6 @@ fi
 DOWNLOAD_DIR="/downloads/yt-dlp"
 mkdir -p "$DOWNLOAD_DIR"
 
-# Create a unique temporary directory for each download
-TMP_DIR=$(mktemp -d)
-
-# Download to the temporary directory and then move the final file
-yt-dlp -o "$TMP_DIR/%(title)s.%(ext)s" "$1"
-
-# Move the completed download to the final destination
-mv "$TMP_DIR"/* "$DOWNLOAD_DIR/"
-
-# Clean up the temporary directory
-rm -rf "$TMP_DIR"
+# Use a unique filename template including the video ID to prevent overwrites
+# and specify the format to ensure a proper video file is downloaded.
+yt-dlp --format 'bestvideo+bestaudio/best' -o "$DOWNLOAD_DIR/%(title)s - [%(id)s].%(ext)s" "$1"
